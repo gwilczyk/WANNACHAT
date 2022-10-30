4d161997516837e1064b1829ca897e67a95fdaf5
GWilczyk <gwilczyk.github@gmail.com>
2022-10-30 16:31:48 +0100

    Add Auth page background image

--- src/pages/auth/AuthTabs/AuthTabs.jsx
+++ src/pages/auth/AuthTabs/AuthTabs.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import backgroundImage from '../../../assets/images/background.jpg'
 import './AuthTabs.scss'
 
 const AuthTabs = () => {
+  const [type, setType] = useState('SignIn')
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    setType((_prevType) => event.target.name)
+  }
+
   return (
-    <div className="container-wrapper">
+    <div className="container-wrapper" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="environment">DEV</div>
 
       <div className="container-wrapper-auth">
         <div className="tabs">
           <div className="tabs-auth">
             <ul className="tab-group">
-              <li className="tab active">
-                <button className="login">Sign In</button>
+              <li className="tab active" onClick={handleClick}>
+                <button className="login" name="SignIn">
+                  Sign In
+                </button>
               </li>
 
-              <li className="tab">
-                <button className="signup">Sign Up</button>
+              <li className="tab" onClick={handleClick}>
+                <button className="signup" name="SignUp">
+                  Sign Up
+                </button>
               </li>
             </ul>
 
-            <div className="tab-item">login component</div>
-            <div className="tab-item">register component</div>
+            {type === 'SignIn' && <div className="tab-item">login component</div>}
+            {type === 'SignUp' && <div className="tab-item">register component</div>}
           </div>
         </div>
       </div>
