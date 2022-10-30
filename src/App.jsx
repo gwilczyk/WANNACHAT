4d457d2e95903a8ab6071a5c242b2d2c40c5ce81
GWilczyk <gwilczyk.github@gmail.com>
2022-10-30 16:15:11 +0100

    App router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.scss'
+import { AppRouter } from './routes'
 
 const App = () => {
   return (
     <>
-      <div>App</div>
+      <Router>
+        <AppRouter />
+      </Router>
     </>
   )
 }
---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import { AuthTabs } from './pages/auth'
+
+export const AppRouter = () => {
+  const elements = useRoutes([
+    {
+      path: '/',
+      element: <AuthTabs />
+    }
+  ])
+
+  return elements
+}
